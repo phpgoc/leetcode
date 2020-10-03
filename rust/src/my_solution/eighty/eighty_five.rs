eff55bf81bd03f8bcce8e8f2df928116dbd65e6c
yangdianqing <phpgoc@icloud.com>
2020-10-04 00:43:47 +0800

    85 maximal_rectangle 优化

--- rust/src/my_solution/eighty/eighty_five.rs
+++ rust/src/my_solution/eighty/eighty_five.rs
@@ -34,14 +34,12 @@ pub fn maximal_rectangle(matrix: Vec<Vec<char>>) -> i32 {
     for y in (0..y_len).rev() {
         for x in (0..x_len).rev() {
             if matrix[y][x] == '1' {
-                let mut cur_ans = 0;
                 let left = left_and_up[y][x].0;
                 let up = left_and_up[y][x].1;
                 if left * up <= ans {
                     continue;
                 }
-                cur_ans = recursive(x, y, &x, &y, left, up, &matrix, &left_and_up, cur_ans);
-                ans = ans.max(cur_ans);
+                ans = recursive(x, y, 0, left, up, &matrix, &left_and_up, ans);
             }
         }
     }
@@ -51,8 +49,7 @@ pub fn maximal_rectangle(matrix: Vec<Vec<char>>) -> i32 {
 fn recursive(
     x: usize,
     y: usize,
-    from_x: &usize,
-    from_y: &usize,
+    level: i32,
     mut max_left: i32,
     mut max_up: i32,
     matrix: &Vec<Vec<char>>,
@@ -60,8 +57,8 @@ fn recursive(
     mut cur_ans: i32,
 ) -> i32 {
     //    println!(
-    //        "x = {} ,y = {},from_x ={},from_y = {}, cur_ans = {}",
-    //        x, y, from_x, from_y, cur_ans
+    //        "x = {} ,y = {},level ={},cur_ans = {}",
+    //        x, y, level, cur_ans
     //    );
 
     let left = left_and_up[y][x].0;
@@ -71,10 +68,11 @@ fn recursive(
     }
     max_left = max_left.min(left);
     max_up = max_up.min(up);
-    let from_to_x = (from_x - x) as i32;
-    let from_to_y = (from_y - y) as i32;
-    cur_ans = cur_ans.max((from_to_x + 1) * (from_to_y + max_up));
-    cur_ans = cur_ans.max((from_to_y + 1) * (from_to_x + max_left));
+    //    if (level + max_up) * (level + max_left) <= cur_ans {
+    //        return cur_ans;
+    //    }
+    cur_ans = cur_ans.max((level + 1) * (level + max_up));
+    cur_ans = cur_ans.max((level + 1) * (level + max_left));
     //    println!("cur_ans = {}", cur_ans);
     if max_left == 1 || max_up == 1 || x == 0 || y == 0 {
         return cur_ans;
@@ -82,8 +80,7 @@ fn recursive(
     cur_ans = recursive(
         x - 1,
         y - 1,
-        from_x,
-        from_y,
+        level + 1,
         max_left - 1,
         max_up - 1,
         matrix,
