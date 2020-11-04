d039c0f30a92649196d000ab9f99d45b12e37930
yangdianqing <phpgoc@icloud.com>
2020-11-04 16:43:22 +0800

    233 count_digit_one

--- rust/src/my_solution/two_hundred/thirty/mod.rs
+++ rust/src/my_solution/two_hundred/thirty/mod.rs
@@ -1,3 +1,4 @@
 #[cfg(test)]
 mod tests;
 pub mod one;
+pub mod three;
--- rust/src/my_solution/two_hundred/thirty/tests.rs
+++ rust/src/my_solution/two_hundred/thirty/tests.rs
@@ -7,3 +7,9 @@ fn is_power_of_two() {
     assert!(one::is_power_of_two(16));
     assert!(!one::is_power_of_two(218));
 }
+#[test]
+fn count_digit_one() {
+    use thirty::three;
+    assert_eq!(three::count_digit_one(13),6);
+    assert_eq!(three::count_digit_one(301203),250645);
+}
