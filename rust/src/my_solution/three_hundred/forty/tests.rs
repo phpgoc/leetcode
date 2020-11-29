9e348adbd28ee1e2eb9b41843df2b92280241886
yangdianqing <phpgoc@icloud.com>
2020-11-29 15:30:52 +0800

    344 reverse_string

--- rust/src/my_solution/three_hundred/forty/tests.rs
+++ rust/src/my_solution/three_hundred/forty/tests.rs
@@ -8,3 +8,11 @@ fn integer_break() {
     use super::three;
     assert_eq!(three::integer_break(10), 36);
 }
+
+#[test]
+fn reverse_string() {
+    use super::four;
+    let mut input = ['h', 'e', 'l', 'l', 'o'].to_vec();
+    four::reverse_string(&mut input);
+    assert_eq!(input, ['o', 'l', 'l', 'e', 'h'].to_vec());
+}
