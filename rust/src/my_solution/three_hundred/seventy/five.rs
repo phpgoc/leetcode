a6ae73cae7a1535c189c94e50fc08551fc78ccd7
yangdianqing <phpgoc@icloud.com>
2020-12-03 18:42:58 +0800

    375 get_money_amount

--- rust/src/my_solution/three_hundred/seventy/five.rs
+++ rust/src/my_solution/three_hundred/seventy/five.rs
@@ -1,10 +1,18 @@
+///我们正在玩一个猜数游戏，游戏规则如下：
+//
+// 我从 1 到 n 之间选择一个数字，你来猜我选了哪个数字。
+//
+// 每次你猜错了，我都会告诉你，我选的数字比你的大了或者小了。
+//
+// 然而，当你猜了数字 x 并且猜错了的时候，你需要支付金额为 x 的现金。直到你猜到我选的数字，你才算赢得了这个游戏。
+//
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/guess-number-higher-or-lower-ii
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 pub fn get_money_amount(n: i32) -> i32 {
     let n = n as usize;
     let mut dp = vec![vec![0; n + 1]; n + 1];
-    for s in 1..n {
-        dp[s][s + 1] = s;
-    }
-    for i in 2..n {
+    for i in 1..n {
         for s in 1..=n - i {
             let mut min = std::usize::MAX;
             for m in s + i / 2..s + i {
