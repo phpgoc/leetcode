7341a877944c9943442b1958cc5e63a1ba481884
phpgoc <phpgoc@gmail.com>
2022-06-30 22:58:04 +0800

    统计295三种方法的运行时间区别

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,5 +1,25 @@
+use std::time::Instant;
+
 #[allow(dead_code)]
 #[allow(unused_parens)]
 #[allow(unused_assignments)]
 pub mod my_solution;
 pub mod no_leetcode;
+
+pub fn cal_runtime(f: fn() -> ()) {
+    let start = Instant::now();
+    f();
+    let end = Instant::now();
+    println!("{:?}", end.duration_since(start));
+}
+#[cfg(test)]
+mod tests{
+    #[test]
+    fn test_calruntime() {
+        super::cal_runtime(|| {
+            let mut v = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+            v.sort();
+            assert_eq!(v, vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+        });
+    }
+}
